ce0870ee991947455baf7b5acfb37ac90e6fc459
DevelopmentTeam <dev@localhost>
2016-12-15 01:56:55 +0900

    -

--- bootstrap.php
+++ bootstrap.php
@@ -4,6 +4,7 @@
     require_once __DIR__."/vendor/autoload.php";
     require_once __DIR__."/config/base.config.php";
     require_once __DIR__."/config/routing.config.php";
-    require_once __DIR__."/config/schema.config.php";
-    require_once __DIR__."/config/env.config.php";
+    if (file_exists(__DIR__."/config/env.config.php")) {
+        require_once __DIR__."/config/env.config.php";
+    }
     app()->init();
