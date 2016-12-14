7fcd8309161615f6946d6282a4f924afcdf20ebd
DevelopmentTeam <dev@localhost>
2016-12-15 03:13:26 +0900

    -

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-    ini_set("display_errors",false);
+    ini_set("display_errors",true);
     error_reporting(E_ALL&~E_NOTICE&~E_STRICT&~E_DEPRECATED);
     require_once __DIR__."/vendor/autoload.php";
     require_once __DIR__."/config/base.config.php";
