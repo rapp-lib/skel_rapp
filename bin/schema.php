5cfbbb421e0cb75f63e523d7bbd02d874494efde
Toyosawa Yasutaka <toyosawa@sharingseed.co.jp>
2017-01-17 03:15:51 +0900

    Update schema.php

--- bin/schema.php
+++ bin/schema.php
@@ -1,7 +1,7 @@
 #!/usr/bin/php
 <?php
     require_once __DIR__."/../bootstrap.php";
-    app()->config(array("Report.force_reporting"=>true));
+    app()->config(array("Config.debug_level"=>true));
     $params =get_cli_params();
 
     foreach (util("Migration")->getMigrateSQL($params["ds"]) as $statement) {
