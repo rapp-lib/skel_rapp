e52a3b8859b0a254a0569b49423152823c04a10f
Toyosawa Yasutaka <toyosawa@sharingseed.co.jp>
2017-08-16 05:31:50 +0900

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -4,6 +4,7 @@
     ini_set("error_reporting", E_ALL&~E_NOTICE&~E_STRICT&~E_DEPRECATED);
     ini_set("date.timezone", "Asia/Tokyo");
     ini_set("mbstring.encoding_translation", false);
+    ini_set("mbstring.internal_encoding", "UTF-8");
     ini_set("auto_detect_line_endings", true);
     ini_set("session.use_trans_sid", false);
     ini_set("session.gc_maxlifetime", 86400);
