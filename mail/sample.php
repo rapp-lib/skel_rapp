962f8773621d4dc4fb7a7df8113878650ebf0222
Toyosawa Yasutaka <toyosawa@sharingseed.co.jp>
2017-08-24 04:55:19 +0900

    Delete sample.php

--- mail/sample.php
+++
@@ -1,7 +0,0 @@
-<?php $mail->import("setup.php"); ?>
-<?php $mail->to("info@example.com"); ?>
-<?php $mail->from("info@example.com"); ?>
-<?php $mail->startBody(); ?>
-subject: Sample-mail
-
-This is Sample.
