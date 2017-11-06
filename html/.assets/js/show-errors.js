c2cbebc8782205bc3389f6dc2f65c6622b141027
toyosawa <toyosawa@sharingseed.co.jp>
2017-11-06 19:50:24 +0900

    RLIB-221

--- html/.assets/js/show-errors.js
+++ html/.assets/js/show-errors.js
@@ -1,7 +1,26 @@
-
 // エラーメッセージを表示する
-window.showErrors = ruiShowErrors;
+window.showErrors = function(o){
+    var errors = o.errors;
+    var $form = o.$form;
+    for (var i in errors) {
+        var $input =$form.find('[name="'+errors[i].name_attr+'"]');
+        var $fieldBlock = $input.closest(".input-area");
+        var msg = "※"+errors[i].message;
+        // 対応するInputがなければ処理しない
+        if ($input.length==0 || $fieldBlock.length==0) continue;
+        // 既存メッセージがあれば重複させない
+        var msg_exists = false;
+        $fieldBlock.find(".errmsg").each(function(){
+            if ($(this).text()==msg) msg_exists = true;
+        });
+        if (msg_exists) continue;
 
+        var $msgbox =$('<p>').addClass('errmsg').text(msg);
+        $fieldBlock.addClass("has-error");
+        $fieldBlock.append($msgbox);
+    }
+};
+// CSSの追加
 appendStyle(
-    '.errmsg { color: #ff4422; } '+
-    '.inputBlock.inputError { background-color: #ffeeee; } ');
+    '.input-area.errmsg { color: #ff4422; } '+
+    '.input-area.has-error { background-color: #ffeeee; } ');
