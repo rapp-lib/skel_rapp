9d6f01c878c639a0589554745fbb8d06b5ad4337
toyosawa <toyosawa@sharingseed.co.jp>
2018-05-18 03:32:11 +0900

    RLIB-432

---
+++ devel/builder/skel/.build_skel.php
@@ -0,0 +1,3 @@
+<?php
+    $skel = include(constant("R_LIB_ROOT_DIR")."/assets/builder/skel/.build_skel.php");
+    return $skel;
---
+++ devel/sandbox/test_bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+    if ( ! defined("R_APP_ROOT_DIR")) require_once __DIR__."/../../bootstrap.php";
+
+class Test_App extends R\Lib\Test\Test_Base
+{
+}
