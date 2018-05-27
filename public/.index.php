dc69c4027eabeadd209f0b7ed5b0701266d3e9a2
toyosawa <toyosawa@sharingseed.co.jp>
2018-05-27 14:01:07 +0900

    RLIB-455

--- public/.index.php
+++ public/.index.php
@@ -1,9 +1,7 @@
 <?php
     ob_start();
     ini_set("display_errors", false);
+    $_ENV["APP_WEBROOT"] = "www";
     require_once __DIR__."/../bootstrap/autoload.php";
     $app = include(constant("R_APP_ROOT_DIR")."/bootstrap/app.php");
-    $response = $app->http->serve("www", function($request){
-        return $request->getUri()->getPageController()->run($request);
-    });
-    $app->http->emit($response);
+    $app->runHttp();
