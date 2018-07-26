1a45d723dfb2aa995a4eb3c779cf06b0fb771804
toyosawa <toyosawa@sharingseed.co.jp>
2018-07-26 20:35:59 +0900

    Bugfix

--- app/Controller/EnumController.php
+++ app/Controller/EnumController.php
@@ -16,7 +16,7 @@ class EnumController extends Controller_App
         $enum = $this->input["enum"];
         $key = $this->input["key"];
         if ( ! in_array($enum, $allow)) return app()->http->response("forbidden");
-        if ( ! strlen($key)) return app("http")->response("json", array());
+        if ( ! $key) return app("http")->response("json", array());
         $value = app()->enum[$enum][$key];
         if (is_array($value)) $value = \R\Lib\Util\Arr::kvdict($value);
         return app("http")->response("json", $value);
