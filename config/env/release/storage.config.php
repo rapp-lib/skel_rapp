ac5006bb9e047be832c0f25f61eb6a14ca8ed82f
dev <test@example.com>
2019-03-29 13:02:50 +0900

    -

--- config/env/release/storage.config.php
+++ config/env/release/storage.config.php
@@ -1,6 +1,6 @@
 <?php
     return array(
-        'db.connection.default.dbname' => "ah113vaphr_toatest",
+        'db.connection.default.dbname' => "ah113vaphr_toadkk",
         'db.connection.default.user' => "ah113vaphr",
         'db.connection.default.password' => "mEiDFWFV",
     );
